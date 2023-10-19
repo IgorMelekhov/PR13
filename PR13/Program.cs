f271ec31417bfa7707c12dd21b0ee8d5d8d10102
IgorMelekhov <143938424+IgorMelekhov@users.noreply.github.com>
2023-10-19 12:26:13 +0300

    Add files via upload

---
+++ PR13/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PR13
+{
+    internal class Program { 
+    public struct Notebook
+    {
+        string surname;
+        string name;
+        ulong phoneNumber;
+        ulong dateOfBirthsday;
+    }
+    public Notebook (ulong phoneNumber)
+    {
+        this.phoneNumber = phoneNumber;
+        surname = null;
+        name = null;
+        dateOfBirth = null;
+        InfoAboutObjStruct();
+    }
+    void InfoAboutObjStruct()
+    {
+        try
+        {
+            Console.Write("\nФамилия:");
+            surname = Console.ReadLine();
+            Console.Write("Имя:");
+            name = Console.ReadLine();
+            Console.Write("Дата рождения:");
+            dateOfBirth= Console.ReadLine();
+            Console.Write("Телефонный номер:");
+            phoneNumber = Console.ReadLine();
+        }catch(FormatException fe)
+        {
+           Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine(fe.Message);
+                Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+        void
+    
+        static void Main(string[] args)
+        {
+        }
+    }
+}
